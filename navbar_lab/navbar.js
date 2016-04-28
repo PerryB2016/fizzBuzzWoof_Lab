9a31ad76d59413ca8a282719bdc62d92ff52e42b
PerryB2016 <perry-brown@sbcglobal.net>
2016-04-28 15:25:23 -0400

    Navbar Lab Submit

--- navbar_lab/navbar.js
+++ navbar_lab/navbar.js
@@ -1,21 +1,25 @@
-//jquery
+
 $(document).ready(function(){
-  $('li').hover(function() {
+  var originalText;
+
+  $("li").hover(function() {
     $(this).fadeTo(100,0.25);
   },
   function() {
-    $(this).fadeTo(100,1);
+    $(this).fadeTo(150,1);
   });
-    var savetext = $(this).text();
-  $('li').click(function(){
+    //var savetext = $(this).text();
+  $("li").click(function(){
       $(this).css("background-color","yellow");
           if ($(this).text() === "clicked"){
             $(this).text(savetext);
-            console.log(savetext);
+            console.log(originalText);
+            $(this).text(originalText);
           }
           else {
+              originalText = $(this).text();
               $(this).text("clicked");
-              console.log("something else");
+              $(this).css("background-color", "yellow");
           }
 });
 });
